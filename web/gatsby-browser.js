0e637a28c7111e1676f5681673eb9b6be1f65c96
Eric Furspan <eric.furspan@gmail.com>
2021-01-21 13:13:25 -0500

    Force window reload onServiceWorkerUpdateReadyt

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -22,11 +22,5 @@ export const wrapPageElement = ({ element, props }) => {
 };
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
+  window.location.reload();
 };
