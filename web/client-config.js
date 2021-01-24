5f2dfe86df8fe9c906b1aa5744c1149ebdb0e1d1
Eric Furspan <eric.furspan@gmail.com>
2021-01-24 15:51:09 -0500

    Change default dataset to test

--- web/client-config.js
+++ web/client-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID || '3z70jizc',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'development',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'test',
   },
   services: {
     gaTrackingId: process.env.GA_TRACKING_ID || 'UA-146550062-3',
