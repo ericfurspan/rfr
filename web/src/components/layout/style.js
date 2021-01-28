93bd2131809464dfa1dfb0af3d50616ecfb20034
Eric Furspan <eric.furspan@gmail.com>
2021-01-28 15:43:12 -0500

    disable backdrop opacity change with nav-menu

--- web/src/components/layout/style.js
+++ web/src/components/layout/style.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const StyledMainContent = styled.main`
   min-height: calc(100vh - 570px); // 570 to account for the footer (450 height + 48 vertical padding + 72 margin-top)
 
+  // disabled this on 1/28/21
   ${(props) => props.hasBackdrop && `
-    opacity: 0.5;
+    // opacity: 0.5;
   `}
 `;
